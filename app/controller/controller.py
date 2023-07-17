1aebb093f43b91f2ab98da1d818856bc83b443c1
Kevin Veen-Birkenbach <kevin@veen.world>
2023-07-17 13:24:57 +0200

    refactored gui

--- app/controller/controller.py
+++ app/controller/controller.py
@@ -1,39 +1,45 @@
-import tkinter as tk
-from tkinter import messagebox, simpledialog
+from PyQt5 import QtWidgets
 from app.model.model import Model
 from app.view.view import View
 
 class Controller:
     def __init__(self):
-        self.window = tk.Tk()
-        self.window.title("Boat Food Inventory Manager")
+        self.app = QtWidgets.QApplication([])
+        self.window = QtWidgets.QMainWindow()
+        self.window.setWindowTitle("Boat Food Inventory Manager")
+
         self.model = Model()
         self.view = View(self.window)
-        self.view.add_button['command'] = self.add_entry
-        self.view.delete_button['command'] = self.delete_entry
-        self.view.save_button['command'] = self.save_changes
-        self.view.reload_button['command'] = self.load_data
+        self.window.setCentralWidget(self.view)
+
+        self.view.add_button.clicked.connect(self.add_entry)
+        self.view.delete_button.clicked.connect(self.delete_entry)
+        self.view.save_button.clicked.connect(self.save_changes)
+        self.view.reload_button.clicked.connect(self.load_data)
+
         self.load_data()
 
     def add_entry(self):
-        name = simpledialog.askstring("Product Name", "Enter the product name")
-        quantity = simpledialog.askstring("Quantity", "Enter the quantity")
-        expiry_date = simpledialog.askstring("Expiry Date", "Enter the expiry date (YYYY-MM-DD)")
-        tags = simpledialog.askstring("Tags", "Enter the tags separated by comma without spaces")
-        self.model.add_entry(name, quantity, expiry_date, tags)
-        self.model.save_changes()
-        self.load_data()
+        name, ok = self.view.get_text_input('Product Name', 'Enter the product name:')
+        quantity, ok = self.view.get_text_input('Quantity', 'Enter the quantity:')
+        expiry_date, ok = self.view.get_text_input('Expiry Date', 'Enter the expiry date (YYYY-MM-DD):')
+        tags, ok = self.view.get_text_input('Tags', 'Enter the tags separated by comma without spaces:')
+        if ok:
+            self.model.add_entry(name, quantity, expiry_date, tags)
+            self.model.save_changes()
+            self.load_data()
 
     def delete_entry(self):
-        del_product = simpledialog.askstring("Product Name", "Enter the name of the product to delete")
-        self.model.delete_entry(del_product)
-        self.model.save_changes()
-        self.load_data()
+        del_product, ok = self.view.get_text_input('Product Name', 'Enter the name of the product to delete:')
+        if ok:
+            self.model.delete_entry(del_product)
+            self.model.save_changes()
+            self.load_data()
 
     def save_changes(self):
         self.model.save_changes()
         self.load_data()
-        messagebox.showinfo("Save Success", "The changes have been successfully saved.")
+        self.view.show_message('Save Success', 'The changes have been successfully saved.')
 
     def load_data(self):
         try:
@@ -43,4 +49,5 @@ class Controller:
             pass
 
     def run(self):
-        self.window.mainloop()
+        self.window.show()
+        self.app.exec_()
--- app/view/view.py
+++ app/view/view.py
@@ -1,26 +1,34 @@
-import tkinter as tk
-from pandastable import Table, TableModel
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QInputDialog, QMessageBox
+import pandas as pd
 
-class View:
-    def __init__(self, window):
-        self.window = window
-        self.table_frame = tk.Frame(window)
-        self.table_frame.pack(fill='both', expand=True)
-        self.add_button = tk.Button(window, text="Add Entry")
-        self.add_button.pack()
-        self.delete_button = tk.Button(window, text="Delete Entry")
-        self.delete_button.pack()
-        self.save_button = tk.Button(window, text="Save Changes")
-        self.save_button.pack()
-        self.reload_button = tk.Button(window, text="Reload Inventory")
-        self.reload_button.pack()
+class View(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.layout = QtWidgets.QVBoxLayout()
+        self.setLayout(self.layout)
+
+        self.table = QtWidgets.QTableWidget()
+        self.layout.addWidget(self.table)
+
+        self.add_button = QtWidgets.QPushButton('Add Entry')
+        self.layout.addWidget(self.add_button)
+
+        self.delete_button = QtWidgets.QPushButton('Delete Entry')
+        self.layout.addWidget(self.delete_button)
+
+        self.save_button = QtWidgets.QPushButton('Save Changes')
+        self.layout.addWidget(self.save_button)
+
+        self.reload_button = QtWidgets.QPushButton('Reload Inventory')
+        self.layout.addWidget(self.reload_button)
 
     def show_table(self, data_frame):
-        for widget in self.table_frame.winfo_children():
-            widget.destroy()
-        table_model = TableModel(data_frame)
-        table = Table(self.table_frame, dataframe=data_frame, model=table_model, showtoolbar=True, showstatusbar=True)
-        table.show()
-        self.window.update_idletasks()  # Add this line
-        self.window.geometry('')  # Reset the geometry
-
+        self.table.setRowCount(0)
+        self.table.setColumnCount(len(data_frame.columns))
+        self.table.setHorizontalHeaderLabels(data_frame.columns)
+        for i, row in data_frame.iterrows():
+            self.table.insertRow(i)
+            for j, cell in enumerate(row):
+                self.table.setItem(i, j, QtWidgets.QTableWidgetItem(str(cell)))
