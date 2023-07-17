9fa7087cc120020599b7ed763594ba30dc320971
Kevin Veen-Birkenbach <kevin@veen.world>
2023-07-17 12:54:03 +0200

    Solved inventory file bug

--- app/model/model.py
+++ app/model/model.py
@@ -1,9 +1,11 @@
+import os
 import pandas as pd
 import json
 
 class Model:
     def __init__(self):
         self.data_frame = pd.DataFrame(columns=['Product Name', 'Quantity', 'Expiry Date', 'Tags'])
+        self.inventory_path = os.path.join(os.path.dirname(__file__), '..', '..', 'data', 'inventory.json')
 
     def add_entry(self, name, quantity, expiry_date, tags):
         self.data_frame.loc[len(self.data_frame)] = [name, quantity, expiry_date, tags]
@@ -13,10 +15,10 @@ class Model:
 
     def save_changes(self):
         data = self.data_frame.to_dict(orient='records')
-        with open('../data/inventory.json', 'w') as json_file:
+        with open(self.inventory_path, 'w') as json_file:
             json.dump(data, json_file, indent=4)
 
     def load_data(self):
-        with open('../data/inventory.json', 'r') as json_file:
+        with open(self.inventory_path, 'r') as json_file:
             data = json.load(json_file)
         self.data_frame = pd.DataFrame(data)
