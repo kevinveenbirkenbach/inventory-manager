fd43e6d7d14fc49296790548065eeb91a5b0708f
Kevin Veen-Birkenbach <kevin@veen.world>
2023-07-17 11:59:01 +0200

    Added table

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import csv
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox, simpledialog
 from csv import DictWriter
-from tkinter import simpledialog
+import pandas as pd
+from pandastable import Table, TableModel
 
 # Main Window
 window = tk.Tk()
@@ -18,17 +19,15 @@ def add_entry():
         dictwriter_object = DictWriter(write_obj, fieldnames=['Product Name', 'Quantity', 'Expiry Date', 'Tags'])
         dictwriter_object.writerow({'Product Name': name, 'Quantity': quantity, 'Expiry Date': expiry_date, 'Tags': tags})
         write_obj.close()
+    load_table()
 
-def display():
-    window = tk.Toplevel()
-    window.title("Display Inventory")
-    with open('boat_food.csv', 'r') as file:
-        reader = csv.reader(file)
-        for i, row in enumerate(reader):
-            for j, col in enumerate(row):
-                label = tk.Label(window, text=col, borderwidth=1)
-                label.grid(row=i, column=j)
-    file.close()
+def load_table():
+    try:
+        data_frame = pd.read_csv('boat_food.csv')
+        table = pt = Table(table_frame, dataframe=data_frame, showtoolbar=True, showstatusbar=True)
+        pt.show()
+    except FileNotFoundError:
+        pass
 
 def delete_entry():
     del_product = simpledialog.askstring("Product Name", "Enter the name of the product to delete")
@@ -43,17 +42,20 @@ def delete_entry():
     with open('boat_food.csv', 'w') as writeFile:
         writer = csv.writer(writeFile)
         writer.writerows(lines)
+    load_table()
 
 # Button to add entries
 add_button = tk.Button(window, text="Add Entry", command=add_entry)
 add_button.pack()
 
-# Button to display entries
-display_button = tk.Button(window, text="Display Inventory", command=display)
-display_button.pack()
-
 # Button to delete entries
 delete_button = tk.Button(window, text="Delete Entry", command=delete_entry)
 delete_button.pack()
+
+# Table Frame
+table_frame = tk.Frame(window)
+table_frame.pack(fill='both', expand=True)
+
+load_table()
 
 window.mainloop()
