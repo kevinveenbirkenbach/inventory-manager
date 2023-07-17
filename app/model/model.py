410369e0b81928bb9d22022c29b4eb1bbd2d104d
Kevin Veen-Birkenbach <kevin@veen.world>
2023-07-17 14:25:28 +0200

    UUID added

--- app/model/model.py
+++ app/model/model.py
@@ -1,17 +1,19 @@
 import os
 import pandas as pd
 import json
+import uuid
 
 class Model:
     def __init__(self):
-        self.data_frame = pd.DataFrame(columns=['Product Name', 'Quantity', 'Expiry Date', 'Tags'])
+        self.data_frame = pd.DataFrame(columns=['ID', 'Product Name', 'Quantity', 'Expiry Date', 'Tags'])
         self.inventory_path = os.path.join(os.path.dirname(__file__), '..', '..', 'data', 'inventory.json')
 
     def add_entry(self, name, quantity, expiry_date, tags):
-        self.data_frame.loc[len(self.data_frame)] = [name, quantity, expiry_date, tags]
+        entry_id = str(uuid.uuid4())
+        self.data_frame.loc[len(self.data_frame)] = [entry_id, name, quantity, expiry_date, tags]
 
-    def delete_entry(self, del_product):
-        self.data_frame = self.data_frame[self.data_frame['Product Name'] != del_product]
+    def delete_entry(self, entry_id):
+        self.data_frame = self.data_frame[self.data_frame['ID'] != entry_id]
 
     def save_changes(self):
         data = self.data_frame.to_dict(orient='records')
